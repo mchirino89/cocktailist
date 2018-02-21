1375c83da914596156fc61325cd79c6df9852407
Mauricio Chirino <mauricio@3codegeeks.com>
2018-02-20 21:32:23 -0300

    Add class for cocktail list

---
+++ Cocktailist/Cocktailist/Controllers/List/CocktailCellTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CocktailCellTableViewCell.swift
+//  Cocktailist
+//
+//  Created by Mauricio Chirino on 20/2/18.
+//  Copyright © 2018 Mauricio Chirino. All rights reserved.
+//
+
+import UIKit
+
+class CocktailCellTableViewCell: UITableViewCell {
+
+}
---
+++ Cocktailist/Cocktailist/Controllers/List/ListController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cocktailist
+//
+//  Created by Mauricio Chirino on 20/2/18.
+//  Copyright © 2018 Mauricio Chirino. All rights reserved.
+//
+
+import UIKit
+
+class ListController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
