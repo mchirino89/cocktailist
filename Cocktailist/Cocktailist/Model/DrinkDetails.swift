90190cafbf12a959d4a55411d5b76fcf5fce938c
Mauricio Chirino <mauricio@3codegeeks.com>
2018-02-27 07:54:51 -0300

    Add custom decodable struct for drink details

--- Cocktailist/Cocktailist/Model/DrinkDetails.swift
+++ Cocktailist/Cocktailist/Model/DrinkDetails.swift
@@ -8,6 +8,23 @@
 
 import Foundation
 
-struct DrinkDetails {
+struct DrinkDetails: Decodable {
+    let drinks: [[String: String?]]
     
+    enum CodingKeys: String, CodingKey {
+        case drinks
+    }
+}
+
+extension DrinkDetails {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        var dictionary: [[String: String?]] = []
+        do {
+            dictionary = try container.decode([[String: String?]].self, forKey: .drinks)
+        } catch {
+            print(error.localizedDescription)
+        }
+        self.init(drinks: dictionary)
+    }
 }
